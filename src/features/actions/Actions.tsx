0294fa4be58a4d73a04e3b5cd310d35c88634d9e
Anastasiia <anastasia.sss.18@gmail.com>
2023-01-16 21:16:57 +0200

    add adaptive

--- src/features/actions/Actions.tsx
+++ src/features/actions/Actions.tsx
@@ -6,7 +6,7 @@ const Actions = () => {
   const {actions} = localization;
 
   return (
-      <div>
+      <div className="actions">
         <div className="item">
           <div><img src={require("../../assets/assets-compressed/actions/miss.png")} alt="miss image"/></div>
           <div className="item__name">{actions.miss}</div>
